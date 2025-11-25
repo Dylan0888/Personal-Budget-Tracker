e01a63224dc9c8db98b9fb968f5935d0357ab51b
Dylan0888 <97961143+Dylan0888@users.noreply.github.com>
2025-11-25 15:57:31 +0000

    set up dark mode functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-
+import { useState } from "react"
+import { MdDarkMode, MdLightMode } from "react-icons/md";
 
 function App() {
+  const [theme , setTheme] = useState("light");
+
+  const toggleTheme = () => {
+      const newTheme = theme === "light" ? "dark" : "light";
+      setTheme(newTheme);
+      document.documentElement.classList.toggle("dark", newTheme === "dark");
+  }
+
+   <button onClick={toggleTheme}>
+      {theme === "light" ? <MdDarkMode/> : <MdLightMode/>}
+    </button>
+
+
+
+
 
 
   return (
     <>
-      
+ 
+   
+  
+
     </>
   )
 }
